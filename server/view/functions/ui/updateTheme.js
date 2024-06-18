ec99c0def8bfe771fe7f7def7c03669ec488e7e4
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-06-17 20:21:49 -0500

    new themes

--- server/view/functions/ui/updateTheme.js
+++ server/view/functions/ui/updateTheme.js
@@ -1,6 +1,6 @@
 const updateTheme = (theme) => {
     const main = document.querySelector('#main')
-    main.classList.remove('ocean_waves', 'turnroot', 'charcoal', 'charcoal_blue', 'charcoal_green', 'chocolate', 'midnight_spark', 'rainforest')
+    main.classList.remove('ocean_waves', 'turnroot', 'charcoal', 'charcoal_blue', 'charcoal_green', 'chocolate', 'midnight_spark', 'snowdrift', 'tokyo_night', 'pink_dream', 'forest_mist', 'sunset_glow', 'pine_coast')
     main.classList.add(theme)
     main.dataset.theme = theme
     window.MainEditorWindow.refresh()
