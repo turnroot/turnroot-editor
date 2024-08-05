d4f92b1af852f0dd26646928dc7ffc5c30cba296
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-08-05 12:55:28 -0500

    cicni

--- server/view/components/iconeditor/top.js
+++ server/view/components/iconeditor/top.js
@@ -23,6 +23,13 @@ let toolbar = new w2toolbar({
             icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>',
         },
         {
+            type: 'button',
+            id: 'search-icon',
+            text: 'Choose icon',
+            class: 'w2ui-btn',
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mouse-pointer-click"><path d="m9 9 5 12 1.8-5.2L21 14Z"/><path d="M7.2 2.2 8 5.1"/><path d="m5.1 8-2.9-.8"/><path d="M14 4.1 12 6"/><path d="m6 12-1.9 2"/></svg>`
+        },
+        {
             type: 'spacer'
         },
         {
@@ -40,9 +47,9 @@ toolbar.on('click', function async (event) {
 
         if (event.detail.item.id === 'new-icon') {
 
-            await window.IconEditorCreateNewIcon(event.detail.value).then(n => {
+            await window.IconEditorCreateNewIcon().then(n => {
                 window.IconEditor.html('main', window.iconEditorEditor)
-
+                window.allIcons.push(n)
                 window.currentIcon = n
 
                 updateCurrentIconRecord(n)
@@ -51,6 +58,18 @@ toolbar.on('click', function async (event) {
             }).catch(e => {
                 console.error(e)
             })
+        } else if (event.detail.item.id === 'search-icon') {
+            console.log(event)
+            if (window.IconPicker) {
+                window.IconPicker.coords = {x: event.detail.originalEvent.clientX, y: event.detail.originalEvent.clientY}
+                window.IconPicker.show()
+                window.currentIcon = await window.IconPicker.icon()
+                updateCurrentIconRecord(window.currentIcon)
+                window.IconEditor.refresh()
+            } else {
+                return w2alert('Error loading IconPicker')
+            }
+            
         } else if (event.detail.item.id === 'delete-icon') {
             if (window.allIcons.length === 0) {
                 return w2alert('No icons to delete')
