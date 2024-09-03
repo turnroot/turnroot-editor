e72d415b622d4803f1ff49ce3310dbc030c05431
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-09-02 20:58:02 -0500

    tilesets skeleton

--- server/view/components/battlefieldeditor/BattlefieldEditor.js
+++ server/view/components/battlefieldeditor/BattlefieldEditor.js
@@ -2,6 +2,7 @@ import { w2layout } from '../../lib/w2ui.es6.min.js'
 import Layers from './canvas/layers.js'
 import tileInfoDiv from './hovering/tileInfo.js'
 import {div, minDiv} from './hovering/layers.js'
+import tilesetsDiv from './hovering/tilesets/tilesets.js'
 import sidebar from './BattlefieldEditorToolbar.js'
 
 import tilesets from '../../style/img/defaultAssets/tilesets/tilesets.json' assert { type: 'json' }
@@ -10,6 +11,7 @@ window.BattlefieldEditorTilesets = {}
 let layers = new Layers(35, 40, 64)
 layers.setLayersDiv(div)
 layers.setLayersDivMin(minDiv)
+layers.setTilesetsDiv(tilesetsDiv)
 layers.addLayer('terrain')
 layers.addLayer('details')
 layers.addLayer('buildings')
--- server/view/components/battlefieldeditor/hovering/tilesets/generate/parse.js
+++ server/view/components/battlefieldeditor/hovering/tilesets/generate/parse.js
@@ -1,4 +1,4 @@
-import { w2alert } from '../../../../../../view/lib/w2ui.es6.min.js'
+import { w2alert, w2toolbar } from '../../../../../../view/lib/w2ui.es6.min.js'
 
 const importTilesets = () => {
     if (!window.BattlefieldEditorTilesetsUnparsed) {
@@ -6,26 +6,37 @@ const importTilesets = () => {
         console.error('window.BattlefieldEditorTilesetsUnparsed is not defined')
         return
     }
+
+    // Initialize BattlefieldEditorTilesetsSheets if not already defined
+    if (!window.BattlefieldEditorTilesetsSheets) {
+        window.BattlefieldEditorTilesetsSheets = {}
+    }
+
+    const fetchPromises = []
+
     try {
+        BattlefieldEditorTilesetsLayoutMain.html('main', 'Loading...')
+        BattlefieldEditorTilesetsLayoutMain.html('bottom', '')
         window.BattlefieldEditorTilesetsUnparsed.forEach(tileset => {
             tileset.sheets.forEach(sheet => {
-                if (window.BattlefieldEditorTilesetsSheets && window.BattlefieldEditorTilesetsSheets[sheet.name]) {
+                if (window.BattlefieldEditorTilesetsSheets[sheet.name]) {
                     console.log('Sheet already loaded')
                     return
                 }
-                fetch(sheet.path)
+
+                const fetchSheetPromise = fetch(sheet.path)
                     .then(response => response.json())
                     .then(data => {
                         console.log(sheet.name)
                         window.BattlefieldEditorTilesetsSheets[sheet.name] = data
-                        let i = -1
-                        data.forEach(tile => {
-                            i++
+
+                        const tileFetchPromises = data.map((tile, i) => {
                             let path = sheet.path.split('/')
                             path.pop()
                             path = path.join('/')
                             path += '/' + tile.name
-                            fetch(path)
+
+                            return fetch(path)
                                 .then(response => response.blob())
                                 .then(blob => {
                                     let url = URL.createObjectURL(blob)
@@ -34,16 +45,100 @@ const importTilesets = () => {
                                 })
                                 .catch(err => console.error(err))
                         })
+
+                        return Promise.all(tileFetchPromises)
                     })
                     .catch(err => console.error(err))
+
+                fetchPromises.push(fetchSheetPromise)
             })
         })
-        window.BattlefieldEditorTilesets = window.BattlefieldEditorTilesetsUnparsed
+
+        Promise.all(fetchPromises)
+            .then(() => {
+                window.BattlefieldEditorTilesets = window.BattlefieldEditorTilesetsUnparsed
+                secondHalf()
+            })
+            .catch(err => console.error(err))
+
     } catch (err) {
         console.error(err)
     }
 }
 
+const secondHalf = () => {
+    let leftHtml = `<h4 style="margin:4px 0padding:4px 0border-bottom:solid 1px var(--list-background)">Tilesets</h4>`
+    let i = -1
+    window.BattlefieldEditorTilesets.forEach(tileset => {
+        i++
+        let style = i === 0
+            ? "background-color:var(--node-title-background)display:flexjustify-content:space-betweenalign-items:center"
+            : "display:flexjustify-content:space-betweenalign-items:center"
+        leftHtml += `<div class="tileset" style="${style}">
+            <p style="margin:0padding:0">${tileset.name}</p>
+        </div>`
+    })
+    window.BattlefieldEditorTilesetsBoxLayout.html('left', leftHtml)
+
+    let toolbarButtons = []
+    i = -1
+    window.BattlefieldEditorTilesets[0].sheets.forEach(sheet => {
+        i++
+        toolbarButtons.push({
+            type: 'radio',
+            group: '1',
+            id: `battlefield-editor-tilesets-toolbar-${sheet.name}`,
+            text: sheet.name,
+            class: 'panel-tabs',
+            checked: i === 0
+        })
+    })
+
+    let toolbar = new w2toolbar({
+        name: 'battlefield-editor-tilesets-toolbar',
+        items: toolbarButtons,
+        onClick(event) {
+            console.log(event)
+        }
+    })
+
+    BattlefieldEditorTilesetsLayoutMain.html('bottom', toolbar)
+
+    if (Object.keys(window.BattlefieldEditorTilesetsSheets).length > 0) {
+        if (window.BattlefieldEditorTilesets) {
+            window.BattlefieldEditorActiveTilesetSheetName = window.BattlefieldEditorTilesets[0].sheets[0].name
+            window.BattlefieldEditorActiveTilesetSheet = window.BattlefieldEditorTilesetsSheets[window.BattlefieldEditorActiveTilesetSheetName]
+
+            let maxColumns = window.BattlefieldEditorTilesets[0].sheets[0].columns
+
+            let tileContainer = document.createElement('div')
+            tileContainer.style.display = 'grid'
+            tileContainer.style.gridTemplateColumns = `repeat(${maxColumns}, 64px)`
+            window.BattlefieldEditorTilesetsBox.style.width = `calc(${maxColumns * 64}px + 7rem)`
+
+            window.BattlefieldEditorActiveTilesetSheet.forEach(tile => {
+                let row = tile.name.split('_')[1]
+                let column = tile.name.split('_')[2]
+                let div = document.createElement('div')
+                div.style.width = '64px'
+                div.style.height = '64px'
+                div.style.backgroundImage = `url(${tile.image})`
+                div.style.backgroundSize = '64px'
+                div.style.cursor = 'pointer'
+
+                div.style.gridColumn = column
+                div.style.gridRow = row
+                tileContainer.appendChild(div)
+            })
+
+            BattlefieldEditorTilesetsLayoutMain.html('main', tileContainer)
+        }
+    } else {
+        console.error('window.BattlefieldEditorTilesetsSheets is not currently defined')
+    }
+}
+
 window.BattlefieldEditorImportTilesets = importTilesets
+window.BattlefieldEditorImportTilesetsSecondHalf = secondHalf
 
 export default importTilesets
